d39d8f600793fd6b4a8ddeb755879471a9e0d104
karakRohan <karakrohan2005@gmail.com>
2026-01-14 00:35:51 +0530

    add code

--- 17_Stack/Lecture - 45/1_Basic_STL.cpp
+++ 17_Stack/Lecture - 45/1_Basic_STL.cpp
@@ -9,12 +9,12 @@ int main()
     stack1.push(20);
     stack1.push(30);
     stack1.push(40);
-    cout << "Size of stack after pushing 4 elements is: " << stack1.size() << endl;
-    stack1.pop();
-    cout << "Size of stack after popping 1 element is: " << stack1.size() << endl;
-    cout << "Top element of stack is: " << stack1.top() << endl;
-    cout<<stack1.empty()<<endl;
-    return 0;
+    // cout << "Size of stack after pushing 4 elements is: " << stack1.size() << endl;
+    // stack1.pop();
+    // cout << "Size of stack after popping 1 element is: " << stack1.size() << endl;
+    // cout << "Top element of stack is: " << stack1.top() << endl;
+    // cout<<stack1.empty()<<endl;
+    // return 0;
 
 
 }
