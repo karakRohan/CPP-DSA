c4c2a4833da18a4aea0bd628765eb719277336c6
karakRohan <karakrohan2005@gmail.com>
2026-01-14 12:18:04 +0530

    new code add

---
+++ 17_Stack/Lecture - 45/2_Stack_Bottom_to_top.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> s;
+    stack<int> temp;
+
+    // Push elements into stack
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+
+    // Reverse the stack
+    while (!s.empty()) {
+        temp.push(s.top());
+        s.pop();
+    }
+
+    // Print bottom to top
+    cout << "Stack elements from bottom to top: ";
+    while (!temp.empty()) {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+
+    return 0;
+}
