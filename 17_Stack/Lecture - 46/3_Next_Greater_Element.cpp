2bd363e4d129898d76ddade10481f22904da02ed
karakRohan <karakrohan2005@gmail.com>
2026-01-31 09:00:09 +0530

    add code

--- 17_Stack/Lecture - 46/3_Next_Greater_Element.cpp
+++ 17_Stack/Lecture - 46/3_Next_Greater_Element.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int arr[] = {3,1,2,7,4,6,2,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" -> ";
+    }
+    cout<<endl;
+    // Next Greater Element
+
 }
