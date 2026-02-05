e23af7f1cd71d17ee58fb4f0c3ab13868e0cfe31
karakRohan <karakrohan2005@gmail.com>
2026-02-05 13:38:16 +0530

    refactor: restructure infix evaluation code for improved readability and organization

--- 17_Stack/Leture - 48/1_Infix_Evaluation.cpp
+++ 17_Stack/Leture - 48/1_Infix_Evaluation.cpp
@@ -1,14 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int priority(char ch)
+{
+    if (ch == '*' || ch == '-')
+        return 1;
+    else
+        return 2; // + , /
+}
+
+int solve(int val1, int val2, char ch)
+{
+    if (ch == '+')
+        return val1 + val2;
+    else if (ch == '-')
+        return val1 - val2;
+    else if (ch == '*')
+        return val1 * val2;
+    else
+        return val1 / val2;
+}
+int main()
+{
     string s = "2+6*4/8-3"; // Infix Expression
     // We need to stacks, 1 for values, 1 for operators
     stack<int> values;
     stack<char> ops;
-    for(int i=0; i<s.length(); i++){
+    for (int i = 0; i < s.length(); i++)
+    {
         // check if s[i] is a digit (0-9)
-        if(s[i]>=48 && s[i]<=57){
-            
+        if (s[i] >= 48 && s[i] <= 57)
+        {                           // Digit
+            values.push(s[i] - 48); // Convert char to int and push into values stack
+        }
+        else
+        { // s[i] it is -> *, /, +, -
+            if (ops.size() == 0 || priority(s[i]) > priority(ops.top()))
+                ops.push(s[i]);
+            else
+            { // Priority (s[i]) <= Priority (ops.top())
+                while (ops.size() > 0 && priority(s[i]) <= priority(ops.top()))
+                {
+                    // i have to do values1 ops values2
+                    char ch = ops.top();
+                    ops.pop();
+                    int val2 = values.top();
+                    values.pop();
+                    int val1 = values.top();
+                    values.pop();
+                    int ans = solve(val1, val2, ch);
+                    values.push(ans);
+                }
+                ops.push(s[i]);
+            }
         }
     }
 }
