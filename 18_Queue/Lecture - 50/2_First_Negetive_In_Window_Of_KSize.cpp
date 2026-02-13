dbfe6965ee3a766603163e1f44b69cc3fc7e264d
karakRohan <karakrohan2005@gmail.com>
2026-02-13 19:33:29 +0530

    add: implement initial setup for first negative in window of k size

---
+++ 18_Queue/Lecture - 50/2_First_Negetive_In_Window_Of_KSize.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {3,-4,-7,30,7,-9,2,1,6,-1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    // -4 -4 -7 -9 -9 -9 0 -1 should be the output
+    vector<int> ans;
+    queue<int> q;
+}
