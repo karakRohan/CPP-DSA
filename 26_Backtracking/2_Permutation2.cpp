246714eb85ac83e2f64e33b00ac4768487cb0c86
karakRohan <karakrohan2005@gmail.com>
2026-04-23 23:57:05 +0530

    add

--- 26_Backtracking/2_Permutation2.cpp
+++ 26_Backtracking/2_Permutation2.cpp
@@ -9,6 +9,8 @@ void permutation(string &str, int i){
     unordered_set<char> s; // to avoid duplicate characters at the same position
     for(int idx=i; idx<str.size(); idx++){
         if(s.count(str[idx])) continue;
+
+        s.insert(str[idx]);
         swap(str[idx], str[i]);
         permutation(str, i+1);
         swap(str[i], str[i]); // backtracking step, to restore the original string for the next iteration
