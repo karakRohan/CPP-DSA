88d4be813f3abfd8d21e50404d513890f2ff4776
karakRohan <karakrohan2005@gmail.com>
2026-04-24 13:52:48 +0530

    add

--- 26_Backtracking/3_N_Queens.cpp
+++ 26_Backtracking/3_N_Queens.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 
 vector<vector<char>> grid;
+vector<vector<string>> result; // to store the final result
 
 bool canPlaceQueen(int row, int col, int n){
     // column check
-    
+    for(int i = row-1; i>= 0; i--){
+        if(grid[i][col] == 'Q') return false; // we are attacked
+    }
+    // left diagonal check
+    for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
+        if(grid[i][j] == 'Q') return false; // we are attacked
+    }
+    // right diagonal check
+    for(int i = row-1, j = col+1; i>=0 && j<n; i--, j++){
+        if(grid[i][j] == 'Q') return false; // we are attacked
+    }
+    return true;  // no attack found
 }
 
 
@@ -13,14 +25,17 @@ bool canPlaceQueen(int row, int col, int n){
 void f(int row, int n){
     // base case
     if(row == n){
+        vector<string> temp;
         // we got one possible ans
         // print the grid (grid display code)
         for(int i=0; i<n; i++){
+            string res = "";
             for(int j=0; j<n; j++){
-                cout << grid[i][j];
+                res += grid[i][j];
             }
-            cout << endl;
+            temp.push_back(res);
         }
+        result.push_back(temp);
         return;
     }
     for(int col=0; col<n; col++){
@@ -30,4 +45,22 @@ void f(int row, int n){
             grid[row][col] = '.'; // backtracking step
         }
     }
+}
+
+
+vector<vector<string>> nqueens(int n) {
+    grid.clear();
+    result.clear();
+    grid.resize(n, vector<char>(n, '.')); // initialize the grid with '.'
+    f(0, n); // start placing queens from the first row
+    return result;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    grid.resize(n, vector<char>(n, '.')); // initialize the grid with '.'
+    f(0, n); // start placing queens from the first row
+    return 0;
 }
