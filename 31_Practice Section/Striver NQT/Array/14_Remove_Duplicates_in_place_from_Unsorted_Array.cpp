924c82ecf43b84bc403443f81d54fdaf532f1423
karakRohan <karakrohan2005@gmail.com>
2026-05-17 00:18:48 +0530

    add

--- 31_Practice Section/Striver NQT/Array/14_Remove_Duplicates_in_place_from_Unsorted_Array.cpp
+++ 31_Practice Section/Striver NQT/Array/14_Remove_Duplicates_in_place_from_Unsorted_Array.cpp
@@ -5,7 +5,6 @@ void removeDuplicate(vector<int>& arr)
 {
     set<int> st;
     vector<int> ans;
-
     for(int i = 0; i < arr.size(); i++)
     {
         if(st.find(arr[i]) == st.end())
@@ -14,19 +13,15 @@ void removeDuplicate(vector<int>& arr)
             ans.push_back(arr[i]);
         }
     }
-
     cout << "{";
-
     for(int i = 0; i < ans.size(); i++)
     {
         cout << ans[i];
-
         if(i != ans.size()-1)
         {
             cout << ",";
         }
     }
-
     cout << "}";
 }
 
@@ -38,17 +33,13 @@ int main(){
     if(s.front()=='{' && s.back()=='}'){
         s = s.substr(1,s.length()-2);
     }
-
     stringstream ss(s);
     string temp;
     vector<int> arr;
-
     while(getline(ss,temp,',')){
         int num = stoi(temp);
         arr.push_back(num);
     }
-
     removeDuplicate(arr);
-
     return 0;
 }
